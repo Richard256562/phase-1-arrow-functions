0362acb1ad865ff92ee98d94915ebad70533587e
RichardOkongo <rirachard19@gmail.com>
2022-12-06 12:34:26 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+// Declare a function using a function expression
+
+const divide = function (){    
+    const num1 = 2000    
+    const num2 = 100    
+    return num1 / num2}
+    divide()
+    const square = (num1) => {    return num1*num1}
+    square()
+    const add = () => {    
+        let a = 3    
+        let b = 4    
+        return a + b
+    }
+
+
+
+
+
+
+
+
+
+
+
+
